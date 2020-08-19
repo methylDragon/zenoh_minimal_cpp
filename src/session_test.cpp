712c71ec0a099e43f71b1be9c48845f7396b4316
methylDragon <methylDragon@gmail.com>
2020-08-20 01:10:02 +0800

    Fix and refactor session test

--- src/session_test.cpp
+++ src/session_test.cpp
@@ -1,21 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
+#include <string>
 
 extern "C" {
   #include "zenoh/zenoh-ffi.h"
 }
 
+std::string data("Hello from C++");
+std::string key("/zenoh/test");
+
 int main(int argc, char** argv) {
-  ZNSession *s = zn_open(CLIENT, "0", 0);
+  ZNSession *s = zn_open(PEER, 0, 0);
   if (s == 0) {
     printf("Error creating session!\n");
     exit(-1);
   }
+
+  if (argc > 1) {
+    key = argv[1];
+  }
+
   sleep(1);
-  const char *data = "Hello from C++";
-  const char *key = "/zenoh/test";
-  zn_write(s, key, data, strlen(data));
+
+  zn_write(s, key.c_str(), data.c_str(), data.length());
   zn_close(s);
 }
